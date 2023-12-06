fb238141d5042748007c337d09f772ebff9bae8d
Resn4R <purview-joust.0x@icloud.com>
2023-12-06 22:49:29 +0000

    Initial Commit

---
+++ AdventOfCodeD6/AdventOfCodeD6App.swift
@@ -0,0 +1,17 @@
+//
+//  AdventOfCodeD6App.swift
+//  AdventOfCodeD6
+//
+//  Created by Vito Borghi on 06/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct AdventOfCodeD6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
